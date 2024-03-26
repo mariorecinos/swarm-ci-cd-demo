cda0abcdbbbb768ec26a388021f9d1c693566fd1
Linden Chiu <lindentree@Lindens-MacBook-Pro.local>
2024-03-26 11:55:52 -0700

    initial

---
+++ src/main/java/com/littletech/swarmrestapi/SwarmRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.littletech.swarmrestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwarmRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwarmRestApiApplication.class, args);
+	}
+
+}
